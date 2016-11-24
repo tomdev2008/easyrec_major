2c9b0b5a488388eab4277b8670b7d9a064a3b4e9
徐绍校 <13554225105@163.com>
2016-11-24 20:34:57 +0800

    推荐系统评测标准

--- easyrec-java8/src/main/java/org/easyrec/java8/domain/Dish.java
+++ easyrec-java8/src/main/java/org/easyrec/java8/domain/Dish.java
@@ -1,8 +1,25 @@
 package org.easyrec.java8.domain;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+
 /**
  * Created by admin on 2016/11/15.
  */
 public class Dish {
-//    private String
+
+    public static void main(String[] args) {
+        List<Integer> someNumbers = Arrays.asList(1, 2, 3, 4, 5);
+        Optional<Integer> firstSquareDivisibleByThree =
+                someNumbers.stream()
+                        .map(x -> x * x)
+                        .filter(x -> x % 3 == 0)
+                        .findFirst(); // 9
+        //规约
+        int num = someNumbers.stream().reduce(0,(a,b) -> a+b);
+        int num2 = someNumbers.stream().reduce(0,Integer::sum);
+        System.out.println(num2);
+    }
+
 }
